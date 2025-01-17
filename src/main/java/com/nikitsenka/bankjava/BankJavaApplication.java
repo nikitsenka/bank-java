eade14a8e592df5915e01075d1175890586ff84a
ivan <nikitsenka@gmail.com>
2025-01-17 09:52:42 +0100

    Add cache

--- src/main/java/com/nikitsenka/bankjava/BankJavaApplication.java
+++ src/main/java/com/nikitsenka/bankjava/BankJavaApplication.java
@@ -4,9 +4,13 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+@EnableCaching
 @SpringBootApplication
 public class BankJavaApplication {
 
@@ -23,5 +27,10 @@ public class BankJavaApplication {
 		dataSource.setConnectionTimeout(20000);
 		dataSource.setMaxLifetime(1200000);
 		return new JdbcTemplate(dataSource);
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager("balances");
 	}
 }
--- src/main/java/com/nikitsenka/bankjava/BankService.java
+++ src/main/java/com/nikitsenka/bankjava/BankService.java
@@ -4,6 +4,9 @@ import com.nikitsenka.bankjava.model.Balance;
 import com.nikitsenka.bankjava.model.Client;
 import com.nikitsenka.bankjava.model.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,11 +22,17 @@ public class BankService {
         repository.createTransaction(new Transaction(0,0, client.getId(), balance));
         return client;
     }
-
+    @Caching(
+            evict = {
+                    @CacheEvict(value = "balances", key = "#transaction.fromClientId"),
+                    @CacheEvict(value = "balances", key = "#transaction.toClientId")
+            }
+    )
     public Transaction newTransaction(Transaction transaction) {
         return repository.createTransaction(transaction);
     }
 
+    @Cacheable(value = "balances", key = "#clientId")
     public Balance getBalance(Integer clientId) {
         return repository.getBalance(clientId);
     }
